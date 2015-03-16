3d03cf1252206935efccea34249b01f6418d9df7
vanbwodonk <arif.pens@gmail.com>
2015-03-16 16:43:20 +0700

    first commit

---
+++ us100.ino
@@ -0,0 +1,36 @@
+/*
+* Code sudah diuji dan ditest oleh iseerobot.com
+*  - TIM QC iseerobot -
+*/
+
+//Wiring : 
+// VCC = 5V
+// GND = GND
+// Trigger = PIN digital 6
+// Echo = PIN digital 7
+
+const int trigger = 6;
+const int echo = 7;
+float distance;
+ 
+void setup(){
+  Serial.begin(9600);
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+}
+void loop(){
+  
+  //Trigger US-100
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  
+  //Tangkap sinyal pantulan dan konversi ke dalam cm
+  distance = pulseIn(echo, HIGH);
+  distance = distance*0.0001657;
+  //kirim hasil lewat usart
+  Serial.println(distance);
+  delay(50);
+}
